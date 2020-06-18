985e4c0c7f38f83e6895c1aa2645b5f3eca08730
Pawan Jenu <pawanjenu@gmail.com>
2020-06-18 10:06:19 +0530

    improved error handling, naming convention and fixed bugs

--- proto/src/main.rs
+++ proto/src/main.rs
@@ -7,9 +7,8 @@ use std::io::prelude::*;
 use std::io::{BufReader, Write};
 
 mod person;
-use person::Person as person_obj;
+use person::Person;
 fn main() {
-
     let matches = App::new("proto")
         .version("1.0")
         .about("example of using protofobuff")
@@ -18,7 +17,7 @@ fn main() {
             Arg::with_name("input")
                 .short("-i")
                 .long("-input_file")
-                .help("path for input file with data")
+                .help("path for input file")
                 .takes_value(true),
         )
         .arg(
@@ -29,26 +28,31 @@ fn main() {
                 .takes_value(true),
         )
         .get_matches();
-    
-    let input_path = matches.value_of("input").unwrap_or("data/input.txt");
-    let output_path = matches.value_of("output").unwrap_or("data/output.txt");
-    let input_file = File::open(input_path).expect("file not found");
-    let input_buffer = BufReader::new(input_file);
-    let mut ofile = File::create(output_path).expect("unable to create file");
 
-    for line in input_buffer.lines() {
-        let each_line = line.unwrap();
-        let mut values: Vec<String> = Vec::new();
-        for i in each_line.split(',') {
-            values.push(i.to_string())
-        }
+    let input_path = matches
+        .value_of("input")
+        .expect("invalid input path argument");
+    let output_path = matches
+        .value_of("output")
+        .expect("invalid output path argument");
+    let input_file = File::open(input_path).expect("input file not found");
+    let input_file_buffer = BufReader::new(input_file);
+    let mut output_file = File::create(output_path).expect("unable to create output file");
 
-        let mut person: person_obj = person_obj::new();
-        person.set_last_name(values[0].to_owned());
-        person.set_first_name(values[1].to_owned());
-        person.set_dob(values[2].to_owned());
+    for line in input_file_buffer.lines() {
+        let each_line = line.expect("unable to read line");
+        let line_info: Vec<&str> = each_line.split(',').collect();
+        let mut person: Person = Person::new();
 
-        let byte_info = person.write_length_delimited_to_bytes().unwrap();
-        ofile.write_all(&byte_info).expect("unable to write");
+        person.set_last_name(line_info[0].to_owned());
+        person.set_first_name(line_info[1].to_owned());
+        person.set_dob(line_info[2].to_owned());
+
+        let person_byte_info = person
+            .write_length_delimited_to_bytes()
+            .expect("unable convert into bytes");
+        output_file
+            .write_all(&person_byte_info)
+            .expect("unable to write to output file");
     }
 }
